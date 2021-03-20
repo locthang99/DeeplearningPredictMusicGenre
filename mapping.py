819ef17a40e530bb240c3e1bbb7d3212228f76ab
Loc Thang <thang040899@gmail.com>
2021-03-20 12:34:56 +0700

    Mapping file

---
+++ mapping.py
@@ -0,0 +1,33 @@
+mappingTypeVietNam = [
+    {'typeId':19,'nametype':'Rap Việt','value':0},
+    {'typeId':15,'nametype':'Cải lương','value':0},
+    {'typeId':10,'nametype':'Nhạc trẻ V-Pop','value':0},
+    {'typeId':14,'nametype':'Nhạc Trịnh','value':0},
+    {'typeId':23,'nametype':'Nhạc trữ tình','value':0},
+    {'typeId':21,'nametype':'Nhạc thiếu nhi','value':0},
+    {'typeId':12,'nametype':'Nhạc cách mạng','value':0},
+    {'typeId':13,'nametype':'Nhạc dân ca','value':0},
+    {'typeId':16,'nametype':'Nhạc không lời','value':0},
+]
+
+mappingTypeAU = [
+    {'typeId':30,'nametype':'Classical','value':0},
+    {'typeId':31,'nametype':'Folk','value':0},
+    {'typeId':32,'nametype':'Country','value':0},
+    {'typeId':33,'nametype':'Rock','value':0},
+    {'typeId':34,'nametype':'Alternative','value':0},
+    {'typeId':35,'nametype':'Rap hip hop','value':0},
+    {'typeId':36,'nametype':'Pop','value':0},
+    {'typeId':37,'nametype':'Latin','value':0},
+    {'typeId':38,'nametype':'Blue jazz','value':0},
+    {'typeId':39,'nametype':'R-B Soul','value':0},
+    {'typeId':40,'nametype':'Reggae','value':0},
+]
+
+list_model =[
+    {'id':0,'name':'model_Res50_VN_7.h5','n_mfcc':13,'country':'VN'},
+    {'id':1,'name':'model_Res50_AU_15.h5','n_mfcc':13,'country':'AU'},
+    {'id':2,'name':'model_Res18_VN_7.h5','n_mfcc':13,'country':'VN'},
+    {'id':3,'name':'model_Res18_VN_7_2.h5','n_mfcc':20,'country':'VN'},
+    {'id':4,'name':'model_Res18_AU_13_2.h5','n_mfcc':20,'country':'AU'},
+]
